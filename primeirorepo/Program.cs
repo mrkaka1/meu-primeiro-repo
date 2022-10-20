ff4455d65d8afc398b74a3895ba01f9a09bf1e4b
mrkaka1 <114708760+mrkaka1@users.noreply.github.com>
2022-10-20 14:50:27 -0400

    Programei a opção 1

--- primeirorepo/Program.cs
+++ primeirorepo/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Olá, Mundo!!!");
+﻿
+
+Console.WriteLine("-----------------------");
+Console.WriteLine("1- Saudações");
+Console.WriteLine("-----------------------");
+
+Console.WriteLine("Escolha uma opção: ");
+int opc = Convert.ToInt32(Console.ReadLine());
+
+if (opc == 1)
+{
+    Console.WriteLine("Qual o seu nome?");
+    string nome= Console.ReadLine();
+
+    Console.WriteLine($"Olá, {nome} !!!");
+}
